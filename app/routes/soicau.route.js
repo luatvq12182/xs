03b2ee23887cf46de2265c0e72ae4cf925e4ac0e
Leo <leo.brightsoft@gmail.com>
2023-12-21 13:20:26 +0700

    api soi cau loto theo thu

--- app/routes/soicau.route.js
+++ app/routes/soicau.route.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const soiCauController = require("../controllers/soicau.controller");
 
 router.get("/loto", soiCauController.loto);
+router.get("/loto-theo-thu", soiCauController.lotoTheoThu);
 
 module.exports = router;
